10dfe132764847f5b7dc7531ca5f78f39c16037e
fahri-rahmanh <fahryrahman162@gmail.com>
2022-08-06 00:19:39 +0700

    adding equip function to character

--- character.py
+++ character.py
@@ -6,5 +6,9 @@ class human:
     def chara(self):
         print("Your health : " + str(self.health))
 
+    def equip(self, weapon):
+        self.weapon = weapon
+        print("You have equipped " + self.weapon)
+
 obj = human(100)
 obj.chara()
