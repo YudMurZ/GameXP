f7aa5c113785b0a9946f30bfb03a7da65025d68e
YudMurZ <68735488+YudMurZ@users.noreply.github.com>
2022-08-09 17:30:45 +0700

    Update database.py

--- database.py
+++ database.py
@@ -2,9 +2,10 @@ from enemy import *
 from weapon import *
 
 weapons = [
-    ['AK-47', 40, 0.55, ],
-    ['M4A1', 25, 0.75],
-    ['MP5', 20, 0.9],
+    ['AK-47', 40, 0.55, 1],
+    ['M4A1', 25, 0.75, 1],
+    ['MP5', 20, 0.9, 1],
+    ['Remington', 5, 0.8, 8]
 ]
 
 enemies = [
