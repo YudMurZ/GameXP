b7e976da54c263840c4d314a7d7666a454209767
fahri-rahmanh <fahryrahman162@gmail.com>
2022-12-06 13:50:50 +0700

    change property visibility in weapon

--- weapon.py
+++ weapon.py
@@ -1,16 +1,29 @@
 class Weapon:
     def __init__(self, data: list):
-        self.name = data[0]
-        self.atk = data[1]
-        self.acc = data[2]
-        self.pellet = data[3]
+        self.__name = data[0]
+        self.__attack = data[1]
+        self.__accuracy = data[2]
+        self.__pellet = data[3]
+
+    def get(self, stat):
+        return self.__dict__['_Weapon__'+stat]
+
+    def fetchStat(self):
+        return {
+            'name': self.__name,
+            'attack': self.__attack,
+            'accuracy': self.__accuracy,
+            'pellet': self.__pellet
+        }
 
     def info(self):
-        print("Weapon Name : " + self.name)
-        print('Attack : ' + self.atk)
-        print('Accuracy : ' + self.acc)
-        print('Pellet Count : ' + self.pellet)
+        print("Weapon Name : " + self.__name)
+        print('Attack : ' + self.__attack)
+        print('Accuracy : ' + self.__accuracy)
+        print('Pellet Count : ' + self.__pellet)
+
 
 # FOR TESTING PURPOSES
-# wpn1 = Weapon('AK-47', 40, 0.55)
+wpn1 = Weapon(['AK-47', 40, 0.55, 1])
+print(wpn1.get('name'))
 # wpn1.info()
